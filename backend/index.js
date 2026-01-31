77c26ec3e6c1807c3193550d5357020a20956bd4
Kleptoman13 <asdfghjkl38939@gmail.com>
2026-01-31 12:49:44 +0100

    First pass for creating todo app

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv" 
+
+import todoRoute from "../backend/routes/todo.route.js"
+
+const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT || 4002
+const DB_URI = process.env.MONGODB_URI
+
+// Database connection code 
+try {
+    await mongoose.connect(DB_URI)
+    console.log("Connected to MongoDB")
+} catch (error) {
+    console.log(error)
+} 
+
+// routes
+app.use(express.json())
+app.use("/todo", todoRoute)
+
+app.listen(PORT, () => {
+    console.log(`Example on post: ${PORT}`)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <div  className="text-xl text-red-500">Hello Dev</div>
+  )
+}
+
+export default App
